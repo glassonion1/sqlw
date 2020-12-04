9806556fef3af6304ac4d730b82c076345496277
taisuke.fujita <glassonion999@gmail.com>
2020-12-04 11:01:39 +0900

    fix unit tests

--- go.mod
+++ go.mod
@@ -3,14 +3,6 @@ module github.com/glassonion1/sqlw
 go 1.15
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/glassonion1/sqlw/sample v0.0.0-20201204012504-52c4d1ea09e3 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/go-cmp v0.5.2
-	github.com/google/uuid v1.1.2 // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/stretchr/testify v1.6.1 // indirect
-	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
 )
--- sqlw_test.go
+++ sqlw_test.go
@@ -11,82 +11,73 @@ import (
 
 //nolint
 func TestMain(m *testing.M) {
-	teardown := setup()
-	defer teardown()
+	setup()
 
 	m.Run()
-
-	// パニックが発生するとteardownが実行されないのでひろう
-	if err := recover(); err != nil {
-		log.Println(err)
-	}
 }
 
-func setup() func() {
+func setup() error {
+	if err := cleanup(); err != nil {
+		return err
+	}
+
 	master, err := sql.Open("mysql", "root:password@tcp(:3306)/app")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer master.Close()
 
-	// テストテーブル3つ users, companies, products
-	// テストデータ2件
+	// Creates three tables users, companies, products
+	// Creates two test data
 
 	ddl1 := `CREATE TABLE IF NOT EXISTS users(
                 id varchar(255),    
                 name varchar(255))`
 	if _, err := master.Exec(ddl1); err != nil {
-		panic(err)
+		return err
 	}
 
 	dml1 := "INSERT INTO users(id, name) VALUES('id_0000', 'hoge')"
 	if _, err := master.Exec(dml1); err != nil {
-		panic(err)
+		return err
 	}
 
 	ddl2 := `CREATE TABLE IF NOT EXISTS companies(
                 id varchar(255),    
                 name varchar(255))`
 	if _, err := master.Exec(ddl2); err != nil {
-		panic(err)
+		return err
 	}
 	dml2 := "INSERT INTO companies(id, name) VALUES('id_0000', 'hoge')"
 	if _, err := master.Exec(dml2); err != nil {
-		panic(err)
+		return err
 	}
 
 	ddl3 := `CREATE TABLE IF NOT EXISTS products(
                 id varchar(255),    
                 name varchar(255))`
 	if _, err := master.Exec(ddl3); err != nil {
-		panic(err)
+		return err
 	}
 
 	log.Println("ready for test")
 
-	// スレーブ遅延対策
+	// For replica rag
 	time.Sleep(time.Second * 1)
 
-	return func() {
-		master, err := sql.Open("mysql", "root:password@tcp(:3306)/app")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer master.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-		ddl1 := "DROP TABLE users"
-		if _, err := master.Exec(ddl1); err != nil {
-			log.Fatal(err)
-		}
-		ddl2 := "DROP TABLE companies"
-		if _, err := master.Exec(ddl2); err != nil {
-			log.Fatal(err)
-		}
-		ddl3 := "DROP TABLE products"
-		if _, err := master.Exec(ddl3); err != nil {
-			log.Fatal(err)
-		}
+	return nil
+}
+
+func cleanup() error {
+	master, err := sql.Open("mysql", "root:password@tcp(:3306)/app")
+	if err != nil {
+		return err
 	}
+	defer master.Close()
+
+	ddl := "DROP TABLE IF EXISTS users, companies, products"
+	if _, err := master.Exec(ddl); err != nil {
+		return err
+	}
+	return nil
 }
